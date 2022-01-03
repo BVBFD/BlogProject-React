5b11d9bd468ed7bf01156dc8ddde85c252715430
BVBFD <lsevina126@gmail.com>
2022-01-03 18:26:15 +0900

    Code Header Part

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./app.css";
+import Home from "./pages/home/home";
 import Topbar from "./topbar/topbar";
 
 const App = (props) => {
   return (
     <>
       <Topbar />
+      <Home />
     </>
   );
 };
---
+++ src/pages/home/home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../../header/header";
+import styles from "./home.module.css";
+
+const Home = (props) => {
+  return (
+    <div className={styles.home}>
+      <Header />
+      homepage
+    </div>
+  );
+};
+
+export default Home;
