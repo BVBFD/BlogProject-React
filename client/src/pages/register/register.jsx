3ae9c0e84df26944c9d4098b4fc9992952c62439
BVBFD <lsevina126@gmail.com>
2022-02-19 02:48:22 +0900

    Code Sign up page connected to backend data

--- client/src/pages/register/register.jsx
+++ client/src/pages/register/register.jsx
@@ -1,37 +1,78 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import styles from "./register.module.css";
 
 const Register = (props) => {
+  const [username, setUsername] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [error, setError] = useState(false);
+
+  console.log(username);
+  console.log(email);
+  console.log(password);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setError(false);
+    try {
+      const res = await axios.post("/auth/register", {
+        username,
+        email,
+        password,
+      });
+      res.data && window.location.replace("/login");
+    } catch (err) {
+      setError(true);
+    }
+  };
+
   return (
     <div className={styles.register}>
       <span className={styles.registerTitle}>Resister</span>
-      <form className={styles.registerForm}>
+      <form className={styles.registerForm} onSubmit={handleSubmit}>
         <label>Username</label>
         <input
           type="text"
           className={styles.registerInput}
           placeholder="Enter your username..."
+          onChange={(event) => {
+            setUsername(event.target.value);
+          }}
         />
         <label>Email</label>
         <input
           type="text"
           className={styles.registerInput}
           placeholder="Enter your email..."
+          onChange={(event) => {
+            setEmail(event.target.value);
+          }}
         />
         <label>Password</label>
         <input
           type="password"
           className={styles.registerInput}
           placeholder="Enter your password..."
+          onChange={(event) => {
+            setPassword(event.target.value);
+          }}
         />
-        <button className={styles.registerButton}>Resister</button>
+        <button className={styles.registerButton} type="submit">
+          Resister
+        </button>
       </form>
       <button className={styles.registerLoginButton}>
         <Link className="link" to="/login">
           Login
         </Link>
       </button>
+      {error && (
+        <span style={{ color: "red", fontWeight: "bold" }}>
+          Something Went Wrong!
+        </span>
+      )}
     </div>
   );
 };
